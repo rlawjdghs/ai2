e1d138d77389dd7b046441faa7527d2a871d4145
rlawjdghs <20242906@sdhs.gwe.hs.kr>
2024-11-18 10:29:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import gdown
 
 # Google Drive 파일 ID
-file_id = '1NKIhMhUeRC0vPptHwT4it-LMYhamVDyi'
+file_id = '1WMPJ14DM6NLk6DxB4uvBaLta20srpVe1'
 
 # Google Drive에서 파일 다운로드 함수
 @st.cache(allow_output_mutation=True)
